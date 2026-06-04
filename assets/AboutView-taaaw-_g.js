580fd800a9b072276f62db923333d4a9bdc5f11d
yong yeon kwon <yongyeonkwon@yongui-MacBookPro.local>
2026-06-04 22:53:04 +0900

    Updates

---
+++ assets/AboutView-taaaw-_g.js
@@ -0,0 +1,1 @@
+import{a as e,i as t,l as n,t as r}from"./index-B5bmAgYI.js";var i={};function a(r,i){return n(),e(`main`,null,[...i[0]||=[t(`h1`,null,`About View`,-1)]])}var o=r(i,[[`render`,a]]);export{o as default};
